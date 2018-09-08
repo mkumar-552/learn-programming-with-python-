84afdc3951a059aa7a1fd6cb3102cf634f36e51d
Manish kumar <30729209+mkumar-552@users.noreply.github.com>
2018-09-08 12:08:39 +0530

    Update planet.py

--- 02-oops/planet.py
+++ 02-oops/planet.py
@@ -8,7 +8,9 @@ class Planet(object):
     def rotate_and_revolve(self):
         self.rotate()
         self.revolve()
-
+    def rotation(self,speed):
+        return (" the rotation of the planet is very {}",format(speed))
 
 earth = Planet()
 earth.rotate_and_revolve()
+print(earth.rotation('fast'))
